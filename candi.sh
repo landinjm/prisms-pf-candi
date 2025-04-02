f355179b3df9f9446709c54a8b241405f07d6435
landinjm <landinjm@umich.edu>
2025-04-02 19:07:36 -0400

    updating partial spack install

--- candi.sh
+++ candi.sh
@@ -79,7 +79,8 @@ version_greater_equal() {
   return 0
 }
 
-check_config_value "USE_SPACK" "$USE_SPACK"
+check_config_value "USE_FULL_SPACK" "$USE_FULL_SPACK"
+check_config_value "USE_PARTIAL_SPACK" "$USE_PARTIAL_SPACK"
 check_config_value "NATIVE_OPTIMIZATIONS" "$NATIVE_OPTIMIZATIONS"
 check_config_value "USE_64BIT_INDICES" "$USE_64BIT_INDICES"
 check_config_value "DEAL_II_WITH_CUDA" "$DEAL_II_WITH_CUDA"
@@ -87,6 +88,15 @@ check_config_value "DEAL_II_WITH_CUDA" "$DEAL_II_WITH_CUDA"
 if ! version_greater_equal "$DEAL_II_VERSION" "9.6"; then
   color_echo ${BAD} "Invalid value for DEAL_II_VERSION=$DEAL_II_VERSION. Expected version greater than 9.6"
   exit 1
+fi
+
+if [ "$USE_FULL_SPACK" == "ON" ] && [ "$USE_PARTIAL_SPACK" == "ON" ]; then
+  color_echo ${BAD} "USE_FULL_SPACK and USE_PARTIAL_SPACK cannot both be ON"
+  exit 1
+elif [ "$USE_FULL_SPACK" == "ON" ] || [ "$USE_PARTIAL_SPACK" == "ON" ]; then
+  USING_SPACK="ON"
+else
+  USING_SPACK="OFF"
 fi
 
 #############################################################
@@ -147,6 +157,7 @@ PREFIX_PATH=${PREFIX/#~\//$HOME\/}
 unset PREFIX
 
 # Set other paths
+SRC_PATH=${PREFIX_PATH}/tmp/src
 BUILD_PATH=${PREFIX_PATH}/tmp/build
 INSTALL_PATH=${PREFIX_PATH}
 
@@ -163,15 +174,15 @@ if ! [ -x "$(command -v git)" ]; then
   color_echo ${BAD} "Make sure git is installed and in path."
   exit 1
 fi
-if ! command -v spack >/dev/null 2>&1 && [ $USE_SPACK == "ON" ]; then
+if ! command -v spack >/dev/null 2>&1 && [ $USING_SPACK == "ON" ]; then
   color_echo ${BAD} "Make sure spack is installed and in path."
   exit 1
 fi
-if ! command -v module >/dev/null 2>&1 && [ $USE_SPACK == "ON" ]; then
+if ! command -v module >/dev/null 2>&1 && [ $USING_SPACK == "ON" ]; then
   color_echo ${BAD} "Make sure spack's module system has been setup and in path."
   exit 1
 fi
-if ! command -v wget >/dev/null 2>&1 && [ $USE_SPACK != "ON" ]; then
+if ! command -v wget >/dev/null 2>&1 && [ $USING_SPACK != "ON" ]; then
   color_echo ${BAD} "Please make sure wget is installed."
   exit 1
 fi
@@ -198,26 +209,31 @@ install_compilers() {
 spack_install_dealii() {
   COMPILER=${COMPILER_TYPE}
 
-  # Package list for the dealii dependencies
-  packages=("dealii@$DEAL_II_VERSION~adol-c~arborx~arpack~assimp~cuda~ginkgo~gmsh~hdf5~metis~muparser~nanoflann~netcdf~oce~opencascade~petsc~scalapack~simplex~slepc~symengine~trilinos~cgal")
   if [ "$DEAL_II_WITH_CUDA" == "ON" ]; then
     color_echo ${BAD} "PRISMS-PF candi does not support spack installation with cuda"
     exit 1
   fi
-  if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
-    packages=("${packages[@]}+gsl")
-  fi
-  if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
-    packages=("${packages[@]}+sundials")
-  fi
-  if [ $USE_64BIT_INDICES == "ON" ]; then
-    packages=("${packages[@]}+int64")
-  fi
-  if [ $NATIVE_OPTIMIZATIONS == "ON" ]; then
-    packages=("${packages[@]}+optflags")
-  fi
-  if [[ " ${PACKAGES[@]} " =~ " caliper " ]]; then
-    packages=("${packages[@]}" "caliper")
+
+  # Package list for the dealii dependencies
+  if [ $USE_FULL_SPACK == "ON" ]; then
+    packages=("dealii@$DEAL_II_VERSION~adol-c~arborx~arpack~assimp~cuda~ginkgo~gmsh~hdf5~metis~muparser~nanoflann~netcdf~oce~opencascade~petsc~scalapack~simplex~slepc~symengine~trilinos~cgal")
+    if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+      packages=("${packages[@]}+gsl")
+    fi
+    if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+      packages=("${packages[@]}+sundials")
+    fi
+    if [ $USE_64BIT_INDICES == "ON" ]; then
+      packages=("${packages[@]}+int64")
+    fi
+    if [ $NATIVE_OPTIMIZATIONS == "ON" ]; then
+      packages=("${packages[@]}+optflags")
+    fi
+    if [[ " ${PACKAGES[@]} " =~ " caliper " ]]; then
+      packages=("${packages[@]}" "caliper")
+    fi
+  else
+    packages=$PACKAGES
   fi
 
   # Install the required and optional dependencies for PRISMS-PF
@@ -236,10 +252,10 @@ spack_install_dealii() {
     fi
 
     # Print the spack concretization
-    spack spec "${packages[@]/%/%${COMPILER_TYPE}@${COMPILER_VERSION}}" >concretization.txt
+    # spack spec "${packages[@]/%/%${COMPILER_TYPE}@${COMPILER_VERSION}}" >concretization.txt
 
-    spack install -j$JOBS "${packages[@]/%/%${COMPILER_TYPE}@${COMPILER_VERSION}}"
-    quit_if_fail "Failed to install required packages"
+    # spack install -j$JOBS "${packages[@]/%/%${COMPILER_TYPE}@${COMPILER_VERSION}}"
+    # quit_if_fail "Failed to install required packages"
     spack load ${packages[@]/%/%${COMPILER_TYPE}@${COMPILER_VERSION}}
   else
     # Print the spack concretization
@@ -250,9 +266,182 @@ spack_install_dealii() {
     spack load ${packages[@]}
   fi
 
+  if [ $USE_PARTIAL_SPACK == "ON" ]; then
+    if [ ! -d "$SRC_PATH/dealii-v$DEAL_II_VERSION" ]; then
+      git clone https://github.com/dealii/dealii.git $SRC_PATH/dealii-v$DEAL_II_VERSION
+      quit_if_fail "Failed to clone dealii"
+    fi
+    cd $SRC_PATH/dealii-v$DEAL_II_VERSION
+    git checkout tags/v$DEAL_II_VERSION
+    quit_if_fail "Invalid version number"
+
+    if [ "$USE_DEFAULT_COMPILER" != "ON" ]; then
+      DEAL_II_BUILD_DIR=$BUILD_PATH/dealii-v$DEAL_II_VERSION-$COMPILER_TYPE-v$COMPILER_VERSION
+      DEAL_II_INSTALL_DIR=$INSTALL_PATH/dealii-v$DEAL_II_VERSION-$COMPILER_TYPE-v$COMPILER_VERSION
+      DEAL_II_MODULE_NAME="$INSTALL_PATH/modules/dealii-v$DEAL_II_VERSION-$COMPILER_TYPE-v$COMPILER_VERSION.lua"
+
+      OPENBLAS_DIR=$(spack location -i $(spack find -H openblas%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      MPI_DIR=$(spack location -i $(spack find -H openmpi%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      P4EST_DIR=$(spack location -i $(spack find -H p4est%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      KOKKOS_DIR=$(spack location -i $(spack find -H kokkos%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      ZLIB_DIR=$(spack location -i $(spack find -H zlib%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+        GSL_DIR=$(spack location -i $(spack find -H gsl%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      fi
+      if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+        SUNDIALS_DIR=$(spack location -i $(spack find -H sundials%${COMPILER_TYPE}@${COMPILER_VERSION} | head -n 1))
+      fi
+    else
+      DEAL_II_BUILD_DIR=$BUILD_PATH/dealii-v$DEAL_II_VERSION
+      DEAL_II_INSTALL_DIR=$INSTALL_PATH/dealii-v$DEAL_II_VERSION
+      DEAL_II_MODULE_NAME="$INSTALL_PATH/modules/dealii-v$DEAL_II_VERSION.lua"
+
+      OPENBLAS_DIR=$(spack location -i $(spack find -H openblas | head -n 1))
+      MPI_DIR=$(spack location -i $(spack find -H openmpi | head -n 1))
+      P4EST_DIR=$(spack location -i $(spack find -H p4est | head -n 1))
+      KOKKOS_DIR=$(spack location -i $(spack find -H kokkos | head -n 1))
+      ZLIB_DIR=$(spack location -i $(spack find -H zlib | head -n 1))
+      if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+        GSL_DIR=$(spack location -i $(spack find -H gsl | head -n 1))
+      fi
+      if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+        SUNDIALS_DIR=$(spack location -i $(spack find -H sundials | head -n 1))
+      fi
+    fi
+
+    if [ ! -d "$DEAL_II_BUILD_DIR" ]; then
+      mkdir $DEAL_II_BUILD_DIR
+    fi
+    cd $DEAL_II_BUILD_DIR
+    cmake_cmd="cmake \
+    -D CMAKE_C_COMPILER=mpicc \
+    -D CMAKE_CXX_COMPILER=mpicxx \
+    -D CMAKE_INSTALL_PREFIX=$DEAL_II_INSTALL_DIR \
+    -D DEAL_II_FORCE_BUNDLED_TBB=ON \
+    -D DEAL_II_WITH_LAPACK=ON \
+    -D LAPACK_DIR=$OPENBLAS_DIR \
+    -D DEAL_II_WITH_MPI=ON \
+    -D MPI_DIR=$MPI_DIR \
+    -D DEAL_II_WITH_P4EST=ON \
+    -D P4EST_DIR=$P4EST_DIR \
+    -D DEAL_II_WITH_KOKKOS=ON \
+    -D KOKKOS_DIR=$KOKKOS_DIR \
+    -D DEAL_II_WITH_ZLIB=ON \
+    -D ZLIB_DIR=$ZLIB_DIR"
+    if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+      cmake_cmd+="-D DEAL_II_WITH_GSL=ON -D GSL_DIR=$GSL_DIR"
+    fi
+    if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+      cmake_cmd+="-D DEAL_II_WITH_SUNDIALS=ON -D SUNDIALS_DIR=$SUNDIALS_DIR"
+    fi
+    if [ $USE_64BIT_INDICES == "ON" ]; then
+      cmake_cmd+="-D DEAL_II_WITH_64BIT_INDICES=ON"
+    fi
+    if [ $NATIVE_OPTIMIZATIONS == "ON" ]; then
+      cmake_cmd+="-D DEAL_II_CXX_FLAGS=\"-march=native\""
+    fi
+
+    cmake_cmd+=" $SRC_PATH/dealii-v$DEAL_II_VERSION"
+    eval $cmake_cmd
+    quit_if_fail "Invalid cmake configuration"
+    make -j$JOBS
+    quit_if_fail "Failed to compile with $(make)"
+    make -j$JOBS install
+    quit_if_fail "Failed to compile with $(make install)"
+    color_echo ${GOOD} "deal.II v${DEAL_II_VERSION} installed"
+
+    if [ ! -d "$INSTALL_PATH/modules" ]; then
+      mkdir "$INSTALL_PATH/modules"
+    fi
+    if [ "$USE_DEFAULT_COMPILER" != "ON" ]; then
+      CMAKE_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" cmake | head -n 1)
+      OPENBLAS_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" openblas | head -n 1)
+      MPI_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" openmpi | head -n 1)
+      P4EST_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" p4est | head -n 1)
+      KOKKOS_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" kokkos | head -n 1)
+      ZLIB_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" zlib | head -n 1)
+      if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+        GSL_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" gsl | head -n 1)
+      fi
+      if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+        SUNDIALS_MODULE=$(spack find --format "{name}/{version}-${COMPILER_TYPE}-${COMPILER_VERSION}" sundials | head -n 1)
+      fi
+
+      cat <<EOF >$DEAL_II_MODULE_NAME
+  -- Module for deal.II v$DEAL_II_VERSION
+
+  -- Set the root path for the module
+  local root = "$DEAL_II_INSTALL_DIR"
+
+  -- Load dependencies
+  if not (isloaded("${COMPILER}/${COMPILER_VERSION}")) then
+      load("${COMPILER}/${COMPILER_VERSION}")
+  end
+  if not (isloaded("$CMAKE_MODULE")) then
+      load("$CMAKE_MODULE")
+  end
+  if not (isloaded("$OPENBLAS_MODULE")) then
+      load("$OPENBLAS_MODULE")
+  end
+  if not (isloaded("$P4EST_MODULE")) then
+      load("$P4EST_MODULE")
+  end
+  if not (isloaded("$KOKKOS_MODULE")) then
+      load("$KOKKOS_MODULE")
+  end
+  if not (isloaded("$ZLIB_MODULE")) then
+      load("$ZLIB_MODULE")
+  end
+EOF
+
+      if [[ " ${PACKAGES[@]} " =~ " gsl " ]]; then
+        cat <<EOF >>$DEAL_II_MODULE_NAME
+  if not (isloaded("$GSL_MODULE")) then
+      load("$GSL_MODULE")
+  end
+EOF
+      fi
+      if [[ " ${PACKAGES[@]} " =~ " sundials " ]]; then
+        cat <<EOF >>$DEAL_II_MODULE_NAME
+  if not (isloaded("$SUNDIALS_MODULE")) then
+      load("$SUNDIALS_MODULE")
+  end
+EOF
+      fi
+
+      cat <<EOF >>$DEAL_II_MODULE_NAME
+  if not (isloaded("$MPI_MODULE")) then
+      load("$MPI_MODULE")
+  end
+    
+  -- Set the environment variables
+  prepend_path("PATH", root.."/bin")
+  prepend_path("LD_LIBRARY_PATH", root.."/lib")
+  prepend_path("CMAKE_PREFIX_PATH", root)
+
+  -- Module load instructions
+  whatis("Name: deal.II")
+  whatis("Version: $DEAL_II_VERSION")
+  whatis("Description: deal.II is a C++ library for finite element methods.")
+EOF
+
+      color_echo ${INFO} "Module file for deal.II v$DEAL_II_VERSION created at $DEAL_II_MODULE_NAME"
+    fi
+  else
+    color_echo ${WARN} "deal.II custom module with default compiler has not been implemented."
+  fi
+
   spack module lmod refresh -y
   spack unload --all
   color_echo ${GOOD} "Required packages installed"
+
+  if [ "$USE_DEFAULT_COMPILER" != "ON" ]; then
+    color_echo ${WARN} "
+The module file has been created, but may not be added to your path. 
+Try adding the following to your environment:
+"
+    color_echo ${INFO} "export MODULEPATH=\$MODULEPATH:$INSTALL_PATH/modules"
+  fi
 }
 
 #############################################################
@@ -397,7 +586,7 @@ install_dealii() {
 #############################################################
 # Start the build process
 
-if [ $USE_SPACK == "ON" ]; then
+if [ $USING_SPACK == "ON" ]; then
   # TODO: If the user desires it we can install spack
 
   # Install compilers with spack
